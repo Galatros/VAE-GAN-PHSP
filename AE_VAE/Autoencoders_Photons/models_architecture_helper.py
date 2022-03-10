af1eb0e876b9034afa192d70f2aad4c9b928b51d
Jakub Michczyński <kubamichcz@gmail.com>
2022-03-10 21:35:53 +0100

    Document used models architecture

--- AE_VAE/Autoencoders_Photons/models_architecture_helper.py
+++ AE_VAE/Autoencoders_Photons/models_architecture_helper.py
@@ -149,3 +149,132 @@ class VAE_Linear_0203(nn.Module):
         encoded = self.reparameterize(z_mean, z_log_var)
         decoded = self.decoder(encoded)
         return encoded, z_mean, z_log_var, decoded
+
+class VAE_Linear_0603(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(6, 5),
+            nn.LeakyReLU(),
+            #nn.BatchNorm1d(5),
+            torch.nn.Linear(5, 4),
+            #nn.BatchNorm1d(4),
+            nn.LeakyReLU(),
+        )
+
+        self.z_mean = nn.Sequential(
+            nn.Linear(4, 4)
+        )
+
+        self.z_log_var = nn.Sequential(
+            nn.Linear(4, 4)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(4, 4),
+            nn.ReLU(),
+            #nn.BatchNorm1d(4),
+            nn.Linear(4, 5),
+            nn.ReLU(),
+            #nn.BatchNorm1d(5),
+            nn.Linear(5, 6)
+        )
+
+    def reparameterize(self, z_mu, z_log_var):
+        eps = torch.randn(z_mu.size(0), z_mu.size(1)).to(
+            z_mu.device)  
+        z = z_mu + eps * torch.exp(z_log_var/2.)
+        return z
+
+    def forward(self, x):
+        x = self.encoder(x)
+        z_mean, z_log_var = self.z_mean(x), self.z_log_var(x)
+        encoded = self.reparameterize(z_mean, z_log_var)
+        decoded = self.decoder(encoded)
+        return encoded, z_mean, z_log_var, decoded
+
+class VAE_Linear_0703(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(6, 5),
+            nn.LeakyReLU(),
+            #nn.BatchNorm1d(5),
+            torch.nn.Linear(5, 5),
+            #nn.BatchNorm1d(4),
+            nn.LeakyReLU(),
+        )
+
+        self.z_mean = nn.Sequential(
+            nn.Linear(5, 4)
+        )
+
+        self.z_log_var = nn.Sequential(
+            nn.Linear(5, 4)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(4, 5),
+            nn.ReLU(),
+            #nn.BatchNorm1d(4),
+            nn.Linear(5, 5),
+            nn.ReLU(),
+            #nn.BatchNorm1d(5),
+            nn.Linear(5, 6)
+        )
+
+    def reparameterize(self, z_mu, z_log_var):
+        eps = torch.randn(z_mu.size(0), z_mu.size(1)).to(
+            z_mu.device)  
+        z = z_mu + eps * torch.exp(z_log_var/2.)
+        return z
+
+    def forward(self, x):
+        x = self.encoder(x)
+        z_mean, z_log_var = self.z_mean(x), self.z_log_var(x)
+        encoded = self.reparameterize(z_mean, z_log_var)
+        decoded = self.decoder(encoded)
+        return encoded, z_mean, z_log_var, decoded
+
+class VAE_Linear_1003(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(6, 5),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(5),
+            torch.nn.Linear(5, 5),
+            nn.BatchNorm1d(5),
+            nn.LeakyReLU(),
+        )
+
+        self.z_mean = nn.Sequential(
+            nn.Linear(5, 4)
+        )
+
+        self.z_log_var = nn.Sequential(
+            nn.Linear(5, 4)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(4, 5),
+            nn.ReLU(),
+            #nn.BatchNorm1d(4),
+            nn.Linear(5, 5),
+            nn.ReLU(),
+            #nn.BatchNorm1d(5),
+            nn.Linear(5, 6)
+        )
+
+    def reparameterize(self, z_mu, z_log_var):
+        eps = torch.randn(z_mu.size(0), z_mu.size(1)).to(
+            z_mu.device)  
+        z = z_mu + eps * torch.exp(z_log_var/2.)
+        return z
+
+    def forward(self, x):
+        x = self.encoder(x)
+        z_mean, z_log_var = self.z_mean(x), self.z_log_var(x)
+        encoded = self.reparameterize(z_mean, z_log_var)
+        decoded = self.decoder(encoded)
+        return encoded, z_mean, z_log_var, decoded
