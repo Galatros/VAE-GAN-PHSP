6fd4c4a32aa59057d62a07d47507af1f3d62e08d
Jakub Michczyński <kubamichcz@gmail.com>
2022-06-28 14:38:04 +0200

    Update mictionary with model names

--- AE_VAE/Lightning_Autoencoders/models/__init__.py
+++ AE_VAE/Lightning_Autoencoders/models/__init__.py
@@ -2,8 +2,16 @@ from .BetaVAE import *
 from .MKMMD_VAE import *
 from .InfoVAE import *
 from .BetaVAE_new import *
+from .MMD_WAE import *
+from .MSSIM_VAE import *
+from .S_WAE import *
+from .MKMMD_InfoVAE import *
 
 vae_models={'BetaVAE':Lit_BetaVAE,
 'MKMMD_VAE': Lit_MKMMD_VAE,
 'InfoVAE': Lit_InfoVAE,
-'BetaVAE_new':Lit_BetaVAE_new,}
+'BetaVAE_new':Lit_BetaVAE_new,
+'MMD_WAE': Lit_MMD_WAE,
+'MSSIM_VAE': Lit_MSSIM_VAE,
+'S_WAE': Lit_S_WAE,
+'MKMMD_InfoVAE': Lit_MKMMD_InfoVAE}
